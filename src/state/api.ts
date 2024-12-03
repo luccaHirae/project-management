17530ec5036ec56a679716027896338de9fd1482
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-03 16:55:42 -0300

    chore: setup redux toolkit

---
+++ src/state/api.ts
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query"; // need to add /react to the end of this import
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [],
+  endpoints: (build) => ({}),
+});
+
+export const {} = api;
