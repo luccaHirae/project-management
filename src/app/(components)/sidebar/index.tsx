47fb5f88fb59fe1a21303ff205d7c2c8f143c277
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-03 17:26:11 -0300

    feat: add theme & sidebar toggle

--- src/app/(components)/sidebar/index.tsx
+++ src/app/(components)/sidebar/index.tsx
@@ -1,16 +1,42 @@
 "use client";
 
-// import { useState } from "react";
+import { useState } from "react";
 import { PROJECT_NAME } from "@/constants";
 import Image from "next/image";
-import { LockIcon } from "lucide-react";
+import {
+  AlertCircle,
+  AlertOctagon,
+  AlertTriangle,
+  Briefcase,
+  ChevronDown,
+  ChevronUp,
+  Home,
+  Layers3,
+  LockIcon,
+  LucideIcon,
+  Search,
+  Settings,
+  ShieldAlert,
+  User,
+  Users,
+  X,
+} from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import Link from "next/link";
+import { setIsSidebarCollapsed } from "@/state";
 
 export const Sidebar = () => {
-  // const [showProjects, setShowProjects] = useState(true);
-  // const [showPriority, setShowPriority] = useState(true);
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
 
   const sidebarClassNames = `fixed flex flex-col h-full justify-between shadow-xl
-    transition-all duration-300 z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+    transition-all duration-300 z-40 dark:bg-black overflow-x-hidden  overflow-y-auto bg-white ${isSidebarCollapsed ? "w-0 hidden" : "w-64"}`;
 
   return (
     <aside className={sidebarClassNames}>
@@ -22,6 +48,17 @@ export const Sidebar = () => {
           >
             {PROJECT_NAME}
           </div>
+
+          {isSidebarCollapsed ? null : (
+            <button
+              onClick={() =>
+                dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+              }
+              className="py-3"
+            >
+              <X className="size-6 text-gray-800 hover:text-gray-500 dark:text-white" />
+            </button>
+          )}
         </div>
 
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
@@ -45,7 +82,100 @@ export const Sidebar = () => {
             </div>
           </div>
         </div>
+
+        <nav className="z-10 w-full">
+          <SidebarLink icon={Home} label="Home" href="/" />
+          <SidebarLink icon={Briefcase} label="Timeline" href="/timeline" />
+          <SidebarLink icon={Search} label="Search" href="/search" />
+          <SidebarLink icon={Settings} label="Settings" href="/settings" />
+          <SidebarLink icon={User} label="Users" href="/users" />
+          <SidebarLink icon={Users} label="Teams" href="/teams" />
+        </nav>
+
+        <button
+          onClick={() => setShowProjects((prev) => !prev)}
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+        >
+          <span className="">Projects</span>
+
+          {showProjects ? (
+            <ChevronUp className="size-5" />
+          ) : (
+            <ChevronDown className="size-5" />
+          )}
+        </button>
+
+        {/* PROJECTS LIST */}
+
+        <button
+          onClick={() => setShowPriority((prev) => !prev)}
+          className="flex w-full items-center justify-between px-8 py-3 text-gray-500"
+        >
+          <span className="">Priority</span>
+
+          {showPriority ? (
+            <ChevronUp className="size-5" />
+          ) : (
+            <ChevronDown className="size-5" />
+          )}
+        </button>
+
+        {showPriority && (
+          <>
+            <SidebarLink
+              icon={AlertCircle}
+              label="Urgent"
+              href="/priority/urgent"
+            />
+            <SidebarLink
+              icon={ShieldAlert}
+              label="High"
+              href="/priority/high"
+            />
+            <SidebarLink
+              icon={AlertTriangle}
+              label="Medium"
+              href="/priority/medium"
+            />
+            <SidebarLink icon={AlertOctagon} label="Low" href="/priority/low" />
+            <SidebarLink
+              icon={Layers3}
+              label="Backlog"
+              href="/priority/backlog"
+            />
+          </>
+        )}
       </div>
     </aside>
+  );
+};
+
+interface SidebarLinkProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+const SidebarLink = ({ href, icon: Icon, label }: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""} justify-start px-8 py-3`}
+      >
+        {isActive && (
+          <div className="absolute left-0 top-0 h-full w-[5px] bg-blue-200"></div>
+        )}
+
+        <Icon className="size-6 text-gray-800 dark:text-gray-100" />
+
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>
+          {label}
+        </span>
+      </div>
+    </Link>
   );
 };
--- src/app/dashboard-wrapper.tsx
+++ src/app/dashboard-wrapper.tsx
@@ -1,21 +1,48 @@
+"use client";
+
 import { Navbar } from "@/app/(components)/navbar";
 import { Sidebar } from "@/app/(components)/sidebar";
+import StoreProvider, { useAppSelector } from "@/app/redux";
+import { useEffect } from "react";
 
 interface DashboardWrapperProps {
   children: React.ReactNode;
 }
 
-export const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
+const DashboardLayout = ({ children }: DashboardWrapperProps) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       <Sidebar />
 
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${
+          isSidebarCollapsed ? "" : "md:pl-64"
+        }`}
       >
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+export const DashboardWrapper = ({ children }: DashboardWrapperProps) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
