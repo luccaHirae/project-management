e0bdcdf607e8b32ff9697170ba95406203fbb8da
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-05 14:47:06 -0300

    chore: move enum to constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,16 @@
 export const PROJECT_NAME = "ETHER";
+
+export enum Status {
+  ToDo = "To Do",
+  WorkInProgress = "Work In Progress",
+  UnderReview = "Under Review",
+  Completed = "Completed",
+}
+
+export enum Priority {
+  Urgent = "Urgent",
+  High = "High",
+  Medium = "Medium",
+  Low = "Low",
+  Backlog = "Backlog",
+}
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,24 +1,11 @@
+import { Priority, Status } from "@/constants";
+
 export interface Project {
   id: number;
   name: string;
   description?: string;
   startDate?: string;
   endDate?: string;
-}
-
-export enum Status {
-  ToDo = "To Do",
-  WorkInProgress = "Work In Progress",
-  UnderReview = "Under Review",
-  Completed = "Completed",
-}
-
-export enum Priority {
-  Urgent = "Urgent",
-  High = "High",
-  Medium = "Medium",
-  Low = "Low",
-  Backlog = "Backlog",
 }
 
 export interface User {
