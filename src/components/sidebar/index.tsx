a812e1ca051924390871ec8dd841833e50da8961
Bruno Hirae <brunodeluca117@outlook.com>
2024-12-05 13:18:30 -0300

    feat: display projects list

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -25,11 +25,13 @@ import { usePathname } from "next/navigation";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import Link from "next/link";
 import { setIsSidebarCollapsed } from "@/state";
+import { useGetProjectsQuery } from "@/state/api";
 
 export const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const { data: projects } = useGetProjectsQuery();
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
@@ -106,6 +108,18 @@ export const Sidebar = () => {
         </button>
 
         {/* PROJECTS LIST */}
+        {showProjects && projects && (
+          <>
+            {projects.map((project) => (
+              <SidebarLink
+                key={project.id}
+                icon={Briefcase}
+                label={project.name}
+                href={`/projects/${project.id}`}
+              />
+            ))}
+          </>
+        )}
 
         <button
           onClick={() => setShowPriority((prev) => !prev)}
